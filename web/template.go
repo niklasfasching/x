d7e02ea9234a12135020a2ec8306244814fa9b1e
Niklas Fasching <niklas.fasching@gmail.com>
2025-10-02 23:30:07 +0200

    web: Improve UX: components, dev hot reload, basic template funcs

--- web/template.go
+++ web/template.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/fs"
+	"log"
 	"net/http"
 	"net/url"
 	"path/filepath"
@@ -15,8 +16,9 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/niklasfasching/x/web/component"
+	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
-	"golang.org/x/net/html"
 )
 
 type Context struct {
@@ -29,6 +31,27 @@ type Context struct {
 
 //go:embed templates.html
 var templates string
+var Funcs = template.FuncMap{
+	"get": func(m map[string]any, k string) map[string]any {
+		if m == nil {
+			return nil
+		} else if _, ok := m[k]; !ok {
+			return nil
+		}
+		return map[string]any{k: m[k]}
+	},
+	"keys": func(m map[string]any) []string { return maps.Keys(m) },
+	"join": strings.Join,
+	"log": func(args ...any) string {
+		log.Println(args...)
+		return ""
+	},
+	"json": func(v any) (string, error) {
+		bs, err := json.MarshalIndent(v, "", "  ")
+		return string(bs), err
+	},
+	"html": func(v string) template.HTML { return template.HTML(v) },
+}
 var TemplateExitErr = fmt.Errorf("render partial template")
 var TemplateHandledErr = fmt.Errorf("skip template rendering")
 var tplExt = ".gohtml"
@@ -38,7 +61,20 @@ var pathPatternRe = regexp.MustCompile(`{(\w+)[.]*}`)
 var camelToKebabRe = regexp.MustCompile("([a-z0-9])([A-Z])")
 
 func TemplateHandler(t *template.Template, tfs fs.FS, dev bool) http.Handler {
-	t = template.Must(t.Parse(templates))
+	if !dev {
+		return templateHandler(t, tfs, dev)
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		templateHandler(t, tfs, dev).ServeHTTP(w, r)
+	})
+}
+
+func templateHandler(t *template.Template, tfs fs.FS, dev bool) http.Handler {
+	t, err := t.Clone()
+	if err != nil {
+		panic(fmt.Errorf("failed to clone base template: %w", err))
+	}
+	t = template.Must(t.New("").Option("missingkey=error").Funcs(Funcs).Parse(templates))
 	ts, err := LoadTemplates(t, tfs, dev)
 	if err != nil {
 		panic(fmt.Errorf("failed to parse templates: %w", err))
@@ -64,7 +100,6 @@ func TemplateHandler(t *template.Template, tfs fs.FS, dev bool) http.Handler {
 }
 
 func LoadTemplates(base *template.Template, tfs fs.FS, includeTest bool) (map[string]*template.Template, error) {
-	base.Funcs(template.FuncMap{"el": func() any { panic("placeholder") }})
 	entrypoints, libs := []string{}, []string{}
 	err := fs.WalkDir(tfs, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil || d.IsDir() || filepath.Ext(path) != tplExt {
@@ -97,32 +132,12 @@ func LoadTemplates(base *template.Template, tfs fs.FS, includeTest bool) (map[st
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse %s: %w", paths, err)
 		}
-		elTpl, err := tpl.Clone()
-		if err != nil {
-			return nil, fmt.Errorf("failed to clone template %s: %w", paths, err)
+		if err := component.Compile(tpl, true); err != nil {
+			return nil, err
 		}
-		tpls[strings.TrimSuffix(path, tplExt)] = tpl.Funcs(template.FuncMap{"el": ElFunc(elTpl)})
+		tpls[strings.TrimSuffix(path, tplExt)] = tpl
 	}
 	return tpls, nil
-}
-
-func ElFunc(t *template.Template) func(name, attrs string, args ...any) (template.HTML, error) {
-	return func(name string, attrs string, args ...any) (template.HTML, error) {
-		t, w := t.Lookup(name), &bytes.Buffer{}
-		if t == nil {
-			return "", fmt.Errorf("el template %q not found", name)
-		}
-		h := html.NewTokenizer(strings.NewReader("<div " + attrs + ">"))
-		if t := h.Next(); t == html.ErrorToken {
-			return "", fmt.Errorf("failed to parse attrs: %w", h.Err())
-		}
-		m := map[string]any{"args": args}
-		for k, v, more := h.TagAttr(); more; k, v, more = h.TagAttr() {
-			m[string(k)] = template.HTMLAttr(v)
-		}
-		err := t.Execute(w, m)
-		return template.HTML(w.String()), err
-	}
 }
 
 func ServeTemplate(t *template.Template, pathKeys []string, w http.ResponseWriter, r *http.Request) {
@@ -159,6 +174,10 @@ func NewContext(t *template.Template, pathKeys []string, w http.ResponseWriter, 
 		c.Form[p] = []string{c.PathValue(p)}
 	}
 	return c, nil
+}
+
+func (c *Context) Get(k string) any {
+	return c.Form.Get(k)
 }
 
 func (c *Context) IsFragment() bool {
